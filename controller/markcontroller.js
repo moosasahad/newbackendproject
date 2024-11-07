f82c22eea5120d9ec895dafa65c541426eecf9ec
moosa sahad <sahadmoosa018@gmail.com>
2024-11-07 17:53:22 +0530

    changed markcontroller

--- controller/markcontroller.js
+++ controller/markcontroller.js
@@ -1,6 +1,6 @@
 const mark = require("../Models/studentMarkSchema");
-const CustomError = require("../utils/CustomError");
-const mongoose = require("mongoose");
+
+const user = require("../Models/userSchema")
 
 // const markcontroller = async (req, res, next) => {
 //   const { TeacherId, ...student } = req.body;
@@ -32,41 +32,117 @@ const mongoose = require("mongoose");
 //     .json({ message: "Mark record created successfully", savedMark });
 // };
 
+// const markcontroller = async (req, res, next) => {
+//   const { TeacherId, ...student } = req.body;
+//   console.log("...student",{ TeacherId, ...student });
+  
+//   const teacher = await user.find({_id:TeacherId}) 
+
+//   if(!teacher){
+//     return res.status(400).json({ message: "user not found." });
+//   }
+
+//   console.log("teacher",teacher);
+
+  
+
+//   if (!student.StudentName || !student.RollNumber || !student.Class || !student.SubjectMarks) {
+//     console.log("student",student.StudentName);
+    
+//     return res.status(400).json({ message: "All student fields must be provided." });
+//   }
+
+
+//   if (!student.devision) {
+//     student.devision = "Default Devision"; 
+//   }
+
+//   console.log("req.body", { TeacherId, ...student });
+
+//   try {
+//     const existingRecord = await mark.findOne({ TeacherId });
+
+//     if (existingRecord) {
+//       const studentToAdd = { ...student };
+
+//       const updatedMark = await mark.findOneAndUpdate(
+//         { TeacherId },
+//         { $push: { student: studentToAdd } },
+//         { new: true }
+//       );
+
+//       return res.status(200).json({
+//         message: "Student added to existing mark record successfully",
+//         updatedMark,
+//       });
+//     }
+
+//     const newMark = new mark({
+//       TeacherId,
+//       student: [student],
+//     });
+
+//     const savedMark = await newMark.save();
+//     return res.status(200).json({ message: "Mark record created successfully", savedMark });
+
+//   } catch (error) {    
+//     console.error("Error:", error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
+
 const markcontroller = async (req, res, next) => {
-  const { TeacherId, ...student } = req.body;
+  const { TeacherId, student } = req.body;
 
-  if (!student.StudentName || !student.RollNumber || !student.Class || !student.SubjectMarks) {
-    return res.status(400).json({ message: "All student fields must be provided." });
+  console.log("Received TeacherId and students:", { TeacherId, student });
+
+ 
+  const teacher = await user.find({ _id: TeacherId });
+
+  if (!teacher || teacher.length === 0) {
+    return res.status(400).json({ message: "User not found." });
+  }
+
+  console.log("teacher found:", teacher);
+
+ 
+  if (!student || student.length === 0) {
+    return res.status(400).json({ message: "No student data provided." });
   }
 
 
-  if (!student.devision) {
-    student.devision = "Default Devision"; 
-  }
+  for (let stu of student) {
+    if (!stu.StudentName || !stu.RollNumber || !stu.Class || !stu.SubjectMarks) {
+      return res.status(400).json({ message: "All student fields must be provided." });
+    }
 
-  console.log("req.body", { TeacherId, ...student });
+    if (!stu.devision) {
+      stu.devision = "Default Division"; 
+    }
+  }
 
   try {
+
     const existingRecord = await mark.findOne({ TeacherId });
 
     if (existingRecord) {
-      const studentToAdd = { ...student };
-
+      
       const updatedMark = await mark.findOneAndUpdate(
         { TeacherId },
-        { $push: { student: studentToAdd } },
+        { $push: { student: { $each: student } } }, 
         { new: true }
       );
 
       return res.status(200).json({
-        message: "Student added to existing mark record successfully",
+        message: "Students added to existing mark record successfully",
         updatedMark,
       });
     }
 
+    
     const newMark = new mark({
       TeacherId,
-      student: [student],
+      student,  
     });
 
     const savedMark = await newMark.save();
@@ -77,6 +153,7 @@ const markcontroller = async (req, res, next) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
 
 
 // get all student mark 
@@ -92,6 +169,6 @@ const getallmark = async (req,res,next)=>{
 
 module.exports = {
   markcontroller, 
-  getallmark,
+  getallmark,     
 };
 
